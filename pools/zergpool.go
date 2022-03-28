59693dd9f9948464d6b3914c8b04404be68ee382
vertiond <vertion@protonmail.com>
2022-03-28 12:32:27 -0500

    Update Zergpool API URL to secured (SSL) connection

--- pools/zergpool.go
+++ pools/zergpool.go
@@ -54,7 +54,7 @@ func (p *Zergpool) GetPayouts(testnet bool) []payouts.Payout {
 
 func (p *Zergpool) GetPendingPayout(addr string) uint64 {
 	jsonPayload := map[string]interface{}{}
-	err := util.GetJson(fmt.Sprintf("http://api.zergpool.com:8080/api/walletEx?address=%s", addr), &jsonPayload)
+	err := util.GetJson(fmt.Sprintf("https://api.zergpool.com:8443/api/walletEx?address=%s", addr), &jsonPayload)
 	if err != nil {
 		return 0
 	}
